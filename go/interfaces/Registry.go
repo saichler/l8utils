5f3c81d7c2cd11738ddcf6a0ccc2ada60e4cda2e
Sharon Aicler <saichler@cisco.com>
2024-11-07 05:00:05 -0800

    Add Registry Interface

---
+++ go/interfaces/Registry.go
@@ -0,0 +1,16 @@
+package interfaces
+
+import (
+	"google.golang.org/protobuf/proto"
+	"reflect"
+)
+
+type IRegistry interface {
+	RegisterStruct(interface{}) bool
+	RegisterStructType(reflect.Type) bool
+	NewProtobufInstance(string) (proto.Message, error)
+	NewInstance(string) (interface{}, error)
+	TypeByName(string) (reflect.Type, error)
+}
+
+var Registry IRegistry
