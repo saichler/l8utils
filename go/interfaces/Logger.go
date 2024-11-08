62175d71745d249837af3053a06678f3fd78e23a
Sharon Aicler <saichler@cisco.com>
2024-11-08 13:24:58 -0800

    Add Logger interface

---
+++ go/interfaces/Logger.go
@@ -0,0 +1,49 @@
+package interfaces
+
+type ILogger interface {
+	Trace(...interface{})
+	Debug(...interface{})
+	Info(...interface{})
+	Warning(...interface{})
+	Error(...interface{}) error
+	Empty() bool
+	Fail(...interface{})
+}
+
+var logger ILogger
+
+func SetLogger(l ILogger) {
+	logger = l
+}
+
+func Logger() ILogger {
+	return logger
+}
+
+func Trace(args ...interface{}) {
+	logger.Trace(args...)
+}
+
+func Debug(args ...interface{}) {
+	logger.Debug(args...)
+}
+
+func Info(args ...interface{}) {
+	logger.Info(args...)
+}
+
+func Warning(args ...interface{}) {
+	logger.Warning(args...)
+}
+
+func Error(args ...interface{}) {
+	logger.Error(args...)
+}
+
+func Empty() bool {
+	return logger.Empty()
+}
+
+func Fail(args ...interface{}) {
+	logger.Fail(args...)
+}
