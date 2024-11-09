eeb958afa56d2948c00da33f9b1757707495131d
Sharon Aicler <saichler@cisco.com>
2024-11-09 08:38:51 -0800

    Add Messages

--- protos/make-bindings.sh
+++ protos/make-bindings.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # Use the protoc image to run protoc.sh and generate the bindings.
-docker run -e PROTO=actions.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
+docker run -e PROTO=request.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
 docker run -e PROTO=priority.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
+docker run -e PROTO=message.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
 
 # Now move the generated bindings to the models directory and clean up
 mkdir -p ../go/types
