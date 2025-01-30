89d89bd34a9799a46911602e496252f5f605faf9
Sharon Aicler <saichler@cisco.com>
2025-01-30 12:31:34 -0800

    Add Notification Type

--- protos/make-bindings.sh
+++ protos/make-bindings.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Use the protoc image to run protoc.sh and generate the bindings.
 docker run -e PROTO=message.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
+docker run -e PROTO=notification.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
 docker run -e PROTO=tests.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
 
 # Now move the generated bindings to the models directory and clean up
