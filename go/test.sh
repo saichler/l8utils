124866774e914248e6eca41091d4a2e9282cf7e4
Sharon Aicler <saichler@cisco.com>
2025-03-06 05:24:14 -0800

    Fix tests

--- go/test.sh
+++ go/test.sh
@@ -2,15 +2,6 @@
 
 # Fail on errors and don't open cover file
 set -e
-# clean up
-rm -rf go.sum
-rm -rf go.mod
-rm -rf vendor
-
-# fetch dependencies
-go mod init
-GOPROXY=direct go mod tidy
-go mod vendor
 
 # Run unit tests with coverage
 go test -v -coverpkg=./share/... -coverprofile=cover-report.html ./... --failfast
