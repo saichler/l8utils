12ab709a215eb5eae4392c3b42c2a0d7aa2735a3
Sharon Aicler <saichler@gmail.com>
2025-11-24 09:31:19 -0800

    Remove debug

--- go/utils/cache/Delete.go
+++ go/utils/cache/Delete.go
@@ -2,7 +2,6 @@ package cache
 
 import (
 	"errors"
-	"runtime/debug"
 
 	"github.com/saichler/l8types/go/types/l8notify"
 )
@@ -27,7 +26,7 @@ func (this *Cache) Delete(v interface{}, createNotification bool) (*l8notify.L8N
 	if this.cacheEnabled() {
 		item, ok = this.iCache.delete(k)
 		if !ok {
-			debug.PrintStack()
+			//debug.PrintStack()
 			return n, errors.New("Delete Key " + k + " not found for " + name)
 		}
 	}
